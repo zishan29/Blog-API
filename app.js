e294b8005d30f4aeb945a8991c6bb6dce3ec32c5
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-01-04 11:14:05 +0530

    Implement CORS

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.use('/', indexRouter);
 
