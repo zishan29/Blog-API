ed49b90b6d152e04b376d70fc17825ad7d3688ac
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-01-04 22:51:13 +0530

    fix CORS

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-app.options('*', cors(corsOptions));
+app.options(cors(corsOptions));
 
 app.use('/', indexRouter);
 
