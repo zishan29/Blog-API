2cd3144ce73457ed7ed622e4eef617deb0a25b81
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-01-04 22:34:07 +0530

    fix CORS

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const Cors = require('micro-cors');
 
 const indexRouter = require('./routes/index');
 
@@ -16,26 +17,22 @@ mongoose.connect(mongoDb);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
 
+const cors = Cors({
+  allowedOrigins: [
+    'https://blog-bice-tau-13.vercel.app',
+    'http://localhost:3000',
+  ],
+  allowedMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    'http://localhost:3000',
-    'https://blog-bice-tau-13.vercel.app/',
-  ];
-  const { origin } = req.headers;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+app.use(cors);
 
 app.use('/', indexRouter);
 
