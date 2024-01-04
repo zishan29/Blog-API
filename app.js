99b8bc27f20904993e8a16609ed48f0ce62afc9f
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-01-04 22:15:10 +0530

    Add CORS for client

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://blog-bice-tau-13.vercel.app/',
+  );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
