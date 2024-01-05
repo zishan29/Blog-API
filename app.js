f02c49a8e9fb810591bb3a7a5e716c2452068a20
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-01-05 22:07:16 +0530

    Attempt 1 to fix CORS issue

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
-app.use('/', indexRouter);
+app.use('/', cors(corsOptions), indexRouter);
 
 module.exports = app;
